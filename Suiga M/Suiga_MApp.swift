ec257c2e859ca7a4a15c4be7a70f3ff5811b50c0
Shin Sie On <coolguysiun@gmail.com>
2023-12-15 23:04:21 +0900

    Initial Commit

---
+++ Suiga M/Suiga_MApp.swift
@@ -0,0 +1,17 @@
+//
+//  Suiga_MApp.swift
+//  Suiga M
+//
+//  Created by 신시언 on 2023/12/15.
+//
+
+import SwiftUI
+
+@main
+struct Suiga_MApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
